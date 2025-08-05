88cf37d81f0fc4dfe937fdc6fe60cda0dedf5937
Anderson Monteiro <alvesmonteiroanderson@gmail.com>
2025-08-05 15:50:38 -0300

    refactor file structure to reflect changes in admin and management commands

--- app/apps/core/management/commands/wait_for_db.py
+++ app/apps/core/management/commands/wait_for_db.py
@@ -1,21 +1,27 @@
+"""
+Django command to wait until database is available.
+"""
 import time
-from django.db import connections
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """Django command to pause execution until database is available"""
-
+    """Django command to wait for database to be available."""
     def handle(self, *args, **options):
-        self.stdout.write('Waiting for database...')
-        db_conn = None
-        while not db_conn:
+        """Entrypoints for command"""
+        self.stdout.write('\nWaiting for database...')
+        db_up = False
+        while db_up is False:
             try:
-                db_conn = connections['default']
-                db_conn.cursor()
-            except OperationalError:
-                self.stdout.write('Database unavailable, waiting 1 second...')
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write(self.style.ERROR('Database not available,'
+                                                   ' waiting for 1 second...'))
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('Database available!')) 
+        self.stdout.write(self.style.SUCCESS('Database is available'))
